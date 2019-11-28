fda5cefc2c521770ae27353d12edbcc5e5af7d05
melisandeteng <melisande.teng@student.ecp.fr>
2019-11-28 18:20:36 -0500

    add dataset from list

--- hourglass/scripts/data.py
+++ hourglass/scripts/data.py
@@ -132,15 +132,100 @@ class HeightDataset(data.Dataset):
             i, j, h, w = self.get_rcrop_params(img)
             img = img[:, i:i+h, j:j+w]
             mask = mask[ :,i:i+h, j:j+w]
-            print(img.shape)
+            #print(img.shape)
             
         # Random vertical flipping
         if random.random() > 0.5:
             img = torch.flip(img, (2, ))
             mask = torch.flip(mask, (2, ))
-            print(img.shape)
+            #print(img.shape)
         return {'image': img, 'mask' : mask}#transforms.ToPILImage()(img.squeeze(0)), 'mask' : mask}
+class HeightDataset_fromlist(data.Dataset):
+    def __init__(
+        self,
+        flist,
+        gtlist, 
+        flist_reader=default_flist_reader,
+        floader=default_loader,
+        gtloader=array_loader,
+        transform_params = None
+    ):
+        self.imlist = flist_reader(flist)
+        self.gtlist = flist_reader(gtlist)
+        self.floader = floader
+        self.gtloader = gtloader
+        self.resize_size = None
+        self.crop_size = None
+        if not transform_params is None : 
+            self.resize_size = transform_params['resize_size']
+            self.crop_size = transform_params['crop_size']      
+        
+    
+    def __getitem__(self, index):
+        impath = self.imlist[index]
+        gtpath = self.gtlist[index]
+        img = self.floader(impath)
+        gt = self.gtloader(gtpath).unsqueeze(0)
+        if not self.transform is None:
+            return(self.transform(img, gt))
+        return {'image': transform.ToTensor()(img), 'mask' : gt}
+    
+    def __len__(self):
+        return (len(self.imlist))
+    
+    def set_transform(self, transform):
+        """This method will replace the current transformation for the
+        dataset.
 
+        :param transform: the new transformation
+        """
+        self.transform = transform
+        
+    def get_rcrop_params(self, img):
+        """Get parameters for ``crop`` for a random crop.
+
+        Args:
+            img (tensor): Image to be cropped.
+            output_size (tuple): Expected output size of the crop.
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
+        """
+        h, w = img.shape[1:]
+        th, tw =  self.crop_size
+        if w == tw and h == th:
+            return (0, 0, h, w)
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return (i, j, th, tw)
+    
+    def transform(self, image, mask):
+
+        image_ = transforms.ColorJitter(brightness=0.5, contrast=0.5)(image)
+        img = transforms.ToTensor()(image_)#.unsqueeze(0)
+        #print(img.shape)
+        if not self.resize_size is None : 
+            # Resize
+  
+            img =  F.interpolate(img, size = self.resize_size, mode = "nearest")
+
+            # h and w are swapped for landmarks because for images,
+            # x and y axes are axis 1 and 0 respectively
+            mask = F.interpolate(mask, size = self.resize_size, mode = "nearest")
+        
+        if not self.crop_size is None :
+            # Random crop
+            i, j, h, w = self.get_rcrop_params(img)
+            img = img[:, i:i+h, j:j+w]
+            mask = mask[ :,i:i+h, j:j+w]
+            #print(img.shape)
+            
+        # Random vertical flipping
+        if random.random() > 0.5:
+            img = torch.flip(img, (2, ))
+            mask = torch.flip(mask, (2, ))
+            #print(img.shape)
+        return {'image': img, 'mask' : mask}
          
 def make_dataset(dir):
     images = []
