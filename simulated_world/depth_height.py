f573a7e37f08b8b08a6daec3d71ad2a9da13188c
melisandeteng <melisande.teng@student.ecp.fr>
2020-01-07 14:10:59 -0500

    add depth, height computation for simulator data

---
+++ simulated_world/depth_height.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import json
+from math import radians, atan, tan, sin, cos
+
+def convert_depth(im_array, far):
+    """
+    convert RGB depth image as np.array to array containing metric depth values.
+    The depth is encoded in the following way: 
+    - The information from the simulator is (1 - LinearDepth (in [0,1])). 
+        far corresponds to the furthest distance to the camera included in the depth map. 
+        LinearDepth * far gives the real metric distance to the camera. 
+    - depth is first divided in 31 slices encoded in R channel with values ranging from 0 to 247
+    - each slice is divided again in 31 slices, whose value is encoded in G channel
+    - each of the G slices is divided into 256 slices, encoded in B channel
+    In total, we have a discretization of depth into N = 31*31*256 - 1 possible values, covering a range of 
+    far/N meters.   
+    Note that, what we encode here is 1 - LinearDepth so that the furthest point is [0,0,0] (that is sky) 
+    and the closest point[255,255,255] 
+    The metric distance associated to a pixel whose depth is (R,G,B) is : 
+        d = (far/N) * [((255 - R)//8)*256*31 + ((255 - G)//8)*256 + (255 - B)]
+                
+    """
+    R = im_array[:,:,0]
+    G = im_array[:,:,1]
+    B = im_array[:,:,2]
+    
+    R = ((247 - R) /8).astype(float)
+    G = ((247 - G)/8).astype(float)
+    B = (255 - B).astype(float)
+    depth = ((R*256*31 + G*256 + B).astype(float))/ (256*31*31 - 1)
+    return(depth*far)
+
+def get_camera_params_from_json(json_file, water_height = 0.45):
+    """
+    We only have access to the absolute height of the camera, not the height wrt ground.
+    Note that ground is not flat in the simulated world. 
+    In our dataset, we collected images such that the water level is always ~0.45 m above ground,
+    the ground reference being where the camera sees. 
+    As an approximation, we will consider this is where the camera sits. 
+    
+    FOV is the vertical field of view, in degrees
+    """
+    with open(json_file) as file:
+        data = json.load(file)
+    FOVy = data['CameraFOV']
+    height = data['CameraPosition']['y'] - data['WaterLevel'] + water_height
+    pitch = 360 - data['CameraRotation']['x']
+    far = data['CameraFar']
+    return {'cam_height': height, 'pitch': pitch, 'FOVy' : FOVy, 'far' : far}
+    
+def get_intrinsic_matrix_vFOV(H,W,FOVy):
+    """ Get matrix of camera intrinsics using vertical field of view
+    
+    Arguments:
+        H {int} -- height of the images, in pixels 
+        W {int} -- width of the images, in pixels 
+        FOVy {float} -- vertical field of view in degrees
+        
+    Returns: 
+        Kc {np.array} -- 3*3 matrix of camera parameters
+       
+    """
+    FOVy = radians(FOVy)
+    FOVx = 2*atan((W/H) * tan(radians(FOVy)/2))
+    cx = (W/2)
+    cy = (H/2)
+
+    fx = cx / tan(FOVx/2)
+    fy = cy/ tan(FOVy/2)
+        #camera intrinsic matrix
+    Kc = np.array([
+                        [fx, 0, cx],
+                        [0, fy, cy],
+                        [0, 0, 1]])
+    return(Kc)
+
+def rotation_matrix_pitch(pitch):
+    """
+    pitch : pitch in degrees
+    """
+    pitch = radians(pitch)
+    return (np.array(
+                        [[1, 0, 0], 
+                         [0, cos(pitch), -sin(pitch)],
+                         [0, sin(pitch), cos(pitch)]]
+                     )
+              )
+
+def get_3D_coords(depth_metric, inv_Kc, camera_height, pitch = 0):
+    """ Get 3D coordinates from metric depth map 
+    
+    Arguments:
+        depth_metric {np.array} -- metric depth map array
+        inv_Kc {float} -- horizontal field of view in degrees
+        pitch {float} -- camera pitch in degrees
+        
+    Returns: 
+        Kc {np.array} -- 3*3 matrix of camera parameters
+       
+    """
+    H, W = depth_metric.shape
+    #get grid of pixel coordinates (2D)
+    coord1, coord2 =  np.mgrid[range(H), range(W)]
+    coords_plane = np.c_[coord2.ravel(), coord1.ravel()]
+    #add third dimension: depth, put minus because in the pinhole camera model the image is upside down
+    coords3 = np.append(coords_plane, -np.expand_dims(depth_metric.flatten(), axis = 1), axis = 1)    
+
+    coords3[:, 0] = np.multiply(coords3[:, 0],coords3[:, 2])
+    coords3[:, 1] = np.multiply(coords3[:, 1],coords3[:, 2])
+
+    #get the coordinates in the camera coordinate system
+    coords_proj = np.transpose(np.matmul(inv_Kc , np.transpose(coords3)))
+    
+    #take into account rotation due to camera pitch (suppose roll is 0 )
+    rotation = rotation_matrix_pitch(pitch)
+    # we actually right apply the transpose of the rotation matrix  which is the same as left applying the transpose matrix 
+    #from camera coordinate system to real
+    coords_proj_ =np.matmul(coords_proj, np.transpose(rotation)) 
+    
+    #pinhole inverses left right too 
+    coords_proj_[:,0] = -coords_proj_[:,0]
+    #add translation (camera level is 0 but in real it's camera_height above ground)
+    coords_proj_[:,1] = coords_proj_[:,1]  + camera_height
+    return(coords_proj_)
+
+def get_height(img_file, json_file, water_height = 0.45):
+    """
+    Arguments:
+        img_file{str} : path to img file
+        json_file{str} : path to json
+        water_height {float} : height of water wrt to ground in cm
+    Returns: 
+        height map np.array
+    """
+    params = get_camera_params_from_json(json_file, water_height)
+    print(params)
+    FOVy = params['FOVy']
+    cam_height =  params['cam_height']
+    pitch = params['pitch']
+    far = params['far']
+    depth = np.array(Image.open(img_file))
+    
+    H,W, _ = depth.shape
+    
+    depth_metric = convert_depth(depth, far)
+    
+    sky0, sky1 = np.where(depth_metric==far)
+    
+    Kc = get_intrinsic_matrix_vFOV(H,W,FOVy)    
+    inv_Kc = np.linalg.inv(Kc)
+    
+    coords = get_3D_coords(depth_metric, inv_Kc, cam_height, pitch)
+    height_array = coords[:, 1].reshape((H,W))
+    
+    #shift heigths to start at 0 
+    #height_array -= np.min(height_array)
+    #clip sky to -1
+    height_array[sky0, sky1] = -1
+
+    return(depth_metric, coords, height_array, params)
+
+def fix_ground_outlier(height_array, seg_array):
+    """
+    look at pixels segmented as water or terrain(later on we would like to merge terrain and ground but for the moment we only 
+    have segmentation maps of flooded images, and look at the height distribution. 
+    Low height outliers of the distribution are clipped to the smallest value in the distribution greater than 
+    mean - standard deviation
+    """
+    #[0,0,255, 255] corresponds to water segmentation, [255, 97, 0, 255] to terrain]
+    indices =  np.where(np.all(seg == [255, 97, 0, 255], axis = -1) | np.all(seg == [0, 0, 255, 255], axis = -1))  
+    m = np.mean(height_array[indices])
+    s = np.std(height_array[indices])
+    min_value = np.min(height_array[height_array < (m - s)])
+    height_array[height_array< (m-s)] = min_value
+    return(height_array)
+    
+############### Visualizations #############################################################################################
+def normalize(array):
+    return((array - np.min(array))/(np.max(input) - np.min(array)))
+
+def plot_logdepth(depth, far): 
+    """
+    Visualize depth map - plot normalized log depth for nicer visuals
+    """
+    im = Image.fromarray(256*(normalize(np.log(depth/far)))).convert('L')
+    fig = plt.imshow(im)
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+
+
+
